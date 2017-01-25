530ff42b056d5a48fa80dbf3f6485f30db4fa3fe
j_polak <jan.polak@onmyway.cz>
2017-01-25 08:42:58 +0100

    Comments & readme

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-week3
+# Week 3 challenge
+
+## Contents
+The `data` folder that will hold all the saved data.
+Make sure that it is writable by PHP in your OS.
+On Mac OSX try this command (while in this folder in the terminal):
+```
+sudo chmod -R a+w data
+```
+
+The `lib` folder containing the library `data-functions.php` that contains all the functions necessary to manipulate saved data
+
+## How to use
+Require the file `lib/data-functions.php` in your script(s).
+
+### get_index
+```
+$index = get_index();
+```
+Returns an array containing pairs of `item id` and `item name`.
+Should be used for creating a list of items.
+
+### get_data
+```
+$data = get_data($id);
+```
+Retrieves saved data for one item based on the given `$id` argument.
+First argument is the id of the item that should be retrieved.
+The returned data is an array containing pairs of `data name` and `data value`.
+
+### insert_data
+```
+insert_data($data);
+```
+Saves the given data as a **new** item.
+The argument should be an array containing pairs of `data name` and `data value`.
+**For proper generating of index, make sure that `data` always contains a value with the key `'name'`.**
+
+### update_data
+```
+update_data($id, $data);
+```
+Updates data for a specific saved item. If that item does not exists a new item is created.
+First argument is the id of the item that should be updated.
+The second argument should be an array containing pairs of `data name` and `data value`.
+
+### delete_data
+```
+delete_data($id);
+```
+Deletes a saved item from the index and deletes it's saved data.
+First argument is the id of the item that should be deleted.
--- lib/data-functions.php
+++ lib/data-functions.php
@@ -4,6 +4,14 @@ define('DATA_DIR', realpath(dirname(__FILE__).'/../data'));
 
 $_data_index = null;
 
+/**
+ * loads the index of all the items into global $_data_index
+ *
+ * loads it from the file DATA_DIR.'/index.csv'
+ * should be called always before working with the index
+ * if the data is already loaded it does not do anything
+ * @return void
+ */
 function load_index()
 {
     global $_data_index;
@@ -23,6 +31,12 @@ function load_index()
     }
 }
 
+/**
+ * saves the current state of the global $_data_index
+ * 
+ * saves it into the file DATA_DIR.'/index.csv'
+ * @return void
+ */
 function save_index()
 {
     load_index();
@@ -38,6 +52,12 @@ function save_index()
     fclose($fh);
 }
 
+/**
+ * gets the current index data
+ *
+ * loads the data first, then returns the globacl $_data_index
+ * @return array - array of the data in the item index
+ */
 function get_index()
 {
     load_index();
@@ -47,11 +67,25 @@ function get_index()
     return $_data_index;
 }
 
+/**
+ * forms a proper filename for storing a data item
+ *
+ * it forms it based on it's id
+ * @param integer $id - id for which the file path should be formed
+ * @return string - the path to the file
+ */
 function get_data_filepath($id)
 {
     return DATA_DIR.'/'.str_pad(floor($id / 1000), 4, STR_PAD_LEFT).'/'.$id.'.json';
 }
 
+/**
+ * gets data for a specific item
+ *
+ * returns empty array if the proper data file is not found or is badly formed
+ * @param integer $id - id of the data item to be retrieved
+ * @return array - an associative array with the item's data or empty array on failure
+ */
 function get_data($id)
 {
     $file = get_data_filepath($id);
@@ -59,12 +93,26 @@ function get_data($id)
     $data = array();
     if(file_exists($file))
     {
-        $data = json_decode(file_get_contents($file));
+        $data = json_decode(file_get_contents($file), true);
+        if($data === false) // json data badly formed
+        {
+            $data = array();
+        }
     }
 
     return $data;
 }
 
+/**
+ * updates data for a specific item
+ *
+ * if the id is empty a new item will be created with the supplied data
+ * if the item does not exist in the index it adds it
+ * if the data file does not exist it creates it
+ * @param integer $id - id of the item to be updated or null to create a new one
+ * @param array $data - an associative array containing all the data for an item
+ * @return integer - the id of the updated/created item
+ */
 function update_data($id = null, $data = array())
 {
     load_index();
@@ -83,7 +131,7 @@ function update_data($id = null, $data = array())
         }
     }
 
-    $name = isset($data['name']) ? $data['name'] : '';
+    $name = isset($data['name']) ? $data['name'] : '-- noname --';
     
     $_data_index[$id] = $name;
 
@@ -99,13 +147,32 @@ function update_data($id = null, $data = array())
     file_put_contents($file, json_encode($data));
 
     chmod($file, 0777);
+
+    return $id;
 }
 
+/**
+ * inserts a new item with the given data
+ *
+ * just a wrapper around calling update_data without an id
+ * @param array $data - an associative array containing all the data for the newly
+ *                      created item
+ * @return integer id of the created item
+ */
 function insert_data($data = array())
 {
-    update_data(null, $data);
+    return update_data(null, $data);
 }
 
+/**
+ * deletes a data item
+ *
+ * removes it from the index and deletes the data file
+ * if such an item does not exist in the index or the data file does not exist,
+ * removes only that which exists
+ * @param integer $id - id of the item to be deleted
+ * @return void
+ */
 function delete_data($id)
 {
     load_index();
